65454497162699e8b29451d1848d1dd0bd139862
Aryan568 <aryanpatidar730@gmail.com>
2023-08-18 22:55:12 +0530

    Library CRUD Application(Android)

--- app/src/main/java/com/example/lms/IssueBook.java
+++ app/src/main/java/com/example/lms/IssueBook.java
@@ -3,11 +3,17 @@ package com.example.lms;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.graphics.Rect;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
+import android.widget.ScrollView;
 
 import com.google.android.material.tabs.TabLayout;
 
 public class IssueBook extends AppCompatActivity {
+
 
     TabLayout tab;
     ViewPager2 pager;
@@ -16,9 +22,12 @@ public class IssueBook extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_issue_book);
-
+        setAdjustScreen();
         tab= findViewById(R.id.tablayout);
         pager= findViewById(R.id.viewpager);
+
+
+
         vpAdatpter= new VPAdatpter(this);
         pager.setAdapter(vpAdatpter);
 
@@ -45,5 +54,10 @@ public class IssueBook extends AppCompatActivity {
                 tab.getTabAt(position).select();
             }
         });
+    }
+
+    private void setAdjustScreen() {
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
     }
 }
